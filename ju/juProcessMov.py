1eacda3b7eb293351e0572b11e288f7c5facd004
jeremyeliosoff <jeremyeliosoff@gmail.com>
2017-09-02 18:27:38 +0530

    juProcessMov.py

--- ju/juProcessMov.py
+++ ju/juProcessMov.py
@@ -4,10 +4,11 @@ import sys, os
 devDir="/home/jeremy/dev/warp/test/"
 sc=.5
 sfx=""
+lev=""
 movFiles = []
 
 if len(sys.argv) == 1:
-	print "\nUSAGE: juProcessMov.py [sc=scale] [sfx=sfx] mov [mov2, mov3..]\n"
+	print "\nUSAGE: juProcessMov.py [sc=scale] [sfx=sfx] [lev=mn,mx,gam] mov [mov2, mov3..]\n"
 	sys.exit()
 
 for arg in sys.argv[1:]:
@@ -15,17 +16,22 @@ for arg in sys.argv[1:]:
 		sc = float(arg.split("=")[1])
 	elif arg[:3] == "sfx":
 		sfx = arg.split("=")[1]
+	elif arg[:3] == "lev":
+		lev = arg.split("=")[1]
 	else:
 		movFiles.append(arg)
 
 print "\nsfx", sfx
 print "\nsc", sc
+print "\nlev", lev
 
 
 for movFile in movFiles:
 	branch = movFile.split("/")[-1]
 	branchWords = branch.split(".")
 	branchRoot = ".".join(branchWords[:-1]) + sfx
+	tmpStr = ".__TMP__"
+	branchRootTmp = branchRoot + tmpStr
 	suffix = branchWords[-1]
 	seqDir = devDir + "seq/" + branchRoot
 	print
@@ -37,15 +43,31 @@ for movFile in movFiles:
 	else:
 		os.makedirs(seqDir)
 		#sysCmd = "ffmpeg -i " + movFile + " -r 60 -vf scale=iw*" + str(sc) + ":ih*" + str(sc) + " -q:v 2 " + seqDir + "/" + branchRoot + ".%04d.jpg"
-		sysCmd = "ffmpeg -i " + movFile + " -vf scale=iw*" + str(sc) + ":ih*" + str(sc) + " -q:v 2 " + seqDir + "/" + branchRoot + ".%05d.jpg"
+		sysCmd = ""
+		thisBranchRoot = branchRoot if lev == "" else branchRootTmp
+		sysCmd += "ffmpeg -i " + movFile + " -vf scale=iw*" + str(sc) + ":ih*" + str(sc) + " -q:v 2 " + seqDir + "/" + thisBranchRoot + ".%05d.jpg"
+
 		print "\n###################################"
 		print "running:", sysCmd
 		print "###################################\n"
 		os.system(sysCmd)
 		print
-		jpgs = os.listdir(seqDir)
-		jpgs.sort()
-		i = 0
+		
+		if not lev == "":
+			jpgs = os.listdir(seqDir)
+			jpgs.sort()
+			i = 0
+			# Color correct.
+			for jpg in jpgs:
+				i += 1
+				mn,mx,gam = lev.split(",")
+				sysCmd = "convert " + seqDir + "/" + jpg + \
+					" -level " + mn + "%," + mx + "%," + gam + " " +\
+					seqDir + "/" + jpg.replace(tmpStr, "") +\
+					"; rm " + seqDir + "/" + jpg
+				print i, "of", str(len(jpgs)) + "; running:", sysCmd
+				os.system(sysCmd)
+
 		# Make textures
 		#for jpg in jpgs:
 		#	i += 1
