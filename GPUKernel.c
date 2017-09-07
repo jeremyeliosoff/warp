8a7affc8830d446550619b7a7c761d90438e634c
jeremyeliosoff <jeremyeliosoff@gmail.com>
2017-09-07 17:22:29 +0530

    getClrAvg.

--- GPUKernel.c
+++ GPUKernel.c
@@ -1,13 +1,34 @@
 
+int getClrAvg(uchar* clr)
+{
+	return (clr[0] + clr[1] + clr[2])/3;
+}
+
+
 void getClr(int x, int y, int xres, int npix,
+  uchar __attribute__((address_space(1)))* imgArray,
+  uchar* ret)
+{
+	int i = y * xres * npix + x * npix;
+	ret[0] = imgArray[i];
+	ret[1] = imgArray[i+1];
+	ret[2] = imgArray[i+2];
+}
+
+
+void cpClr(int x, int y, int xres, int npix,
   uchar __attribute__((address_space(1)))* imgArray,
   uchar __attribute__((address_space(1)))* ret)
 {
 	int i = y * xres * npix + x * npix;
-	ret[i] = imgArray[i];
-	ret[i+1] = imgArray[i+1];
-	ret[i+2] = imgArray[i+2];
+	uchar clr[3];
+	getClr(x, y, xres, npix, imgArray, clr);
+	uchar avg = getClrAvg(clr);
+	ret[i] = avg;
+	ret[i+1] = avg;
+	ret[i+2] = avg;
 }
+
 __kernel void initJtC(
 			__global uchar* imgArray,
 			__global uchar* jtLevels) 
@@ -18,6 +39,6 @@ __kernel void initJtC(
 	int xres = %d;
 	int yres = %d;
 	int npix = 3;
-	getClr(x, y, xres, npix, imgArray, jtLevels);
+	cpClr(x, y, xres, npix, imgArray, jtLevels);
 
 }
