364c68b9e17327012550743b788d3973cf23458b
jeremyeliosoff <jeremyeliosoff@gmail.com>
2017-09-25 17:51:33 +0530

    Start making functions, blit splites in separate loops.

--- makeSpriteImgs.py
+++ makeSpriteImgs.py
@@ -7,10 +7,10 @@ import numpy as np
 #dataV = "v021"
 #frange = range(5,6):
 #lrange = range(0,1):
-#seq = "lzSmokeShortp2pngGm1p3"
-#dataV = "v063"
-seq = "lzSmokeShortFRpng"
-dataV = "v012GPUNewHope"
+seq = "lzSmokeShortp2pngGm1p3"
+dataV = "v063"
+#seq = "lzSmokeShortFRpng"
+#dataV = "v012GPUNewHope"
 frange = range(250,251)
 lrange = range(0,10)
 
@@ -67,94 +67,118 @@ invert255V = np.vectorize(invert255)
 
 
 
-for fr in frange:
-	print "Doing fr", fr, "..."
-	tidToSidsPath = "/home/jeremy/dev/warp/data/" + seq + "/" + dataV + "/frames/%05d/tidToSids" % fr
-	tidToSids = genData.pickleLoad(tidToSidsPath)
+def genAndRenSprites():	
+	for fr in frange:
+		print "Doing fr", fr, "..."
+		tidToSidsPath = "/home/jeremy/dev/warp/data/" + seq + "/" + dataV + "/frames/%05d/tidToSids" % fr
+		tidToSids = genData.pickleLoad(tidToSidsPath)
 
-	base = "/home/jeremy/dev/warp/data/" + seq + "/" + dataV + "/frames/%05d" % fr
-	tidPosGridPath = base + "/tidPosGrid"
-	tidPosGrid = genData.pickleLoad(tidPosGridPath)
+		base = "/home/jeremy/dev/warp/data/" + seq + "/" + dataV + "/frames/%05d" % fr
+		tidPosGridPath = base + "/tidPosGrid"
+		tidPosGrid = genData.pickleLoad(tidPosGridPath)
 
-	srcPath = "/home/jeremy/dev/warp/seq/" + seq + "/" + seq + ".%05d.png" % fr
-	srcImg = pygame.image.load(srcPath)
-	res = srcImg.get_size()
+		srcPath = "/home/jeremy/dev/warp/seq/" + seq + "/" + seq + ".%05d.png" % fr
+		srcImg = pygame.image.load(srcPath)
+		res = srcImg.get_size()
 
 # Initialize canvas
+		#canvas = pygame.Surface(res, pygame.SRCALPHA, 32)
+		#canvas.fill((0, 90, 0))
+
+		spritesThisFr = []
+		for lev in lrange:
+			print "Doing lev", lev, "..."
+
+			tidPosGridThisLev = np.array(tidPosGrid[lev])
+			tidToSidsThisLev = tidToSids[lev]
+
+			# Pad to fit img dimensions - no tid for last x or y.
+			tidPosGridThisLev = np.lib.pad(tidPosGridThisLev, ((1,0), (0,1)), "constant", constant_values=-1)
+			tids = tidToSidsThisLev.keys()
+			tids.sort()
+
+			tidClrGrid = converTidPosGridToTidClrGrid(tidPosGridThisLev, tids)
+			tidImg = pygame.surfarray.make_surface(tidClrGrid)
+			path = "test/img/tidImg.lev%03d.%05d.png" % (lev, fr)
+			pygame.image.save(tidImg, path)
+
+			# Initialize canvasLev
+			#canvasLev = pygame.Surface(res, pygame.SRCALPHA, 32)
+			#canvasLev.fill((0, 90, 0))
+
+			spritesThisLev = []
+
+			for tidPos in range(len(tids)):
+				tid = tids[tidPos]
+				bbx = tidToSidsThisLev[tid]["bbx"]
+				sz=(bbx[1][0]-bbx[0][0], bbx[1][1]-bbx[0][1])
+
+				# +1 totally trial + error. ***********???????? maybe not needed?
+				bbxTup = (bbx[0][0]+1, bbx[0][1]+1, bbx[1][0], bbx[1][1])
+				#print "bbxTup", bbxTup
+
+				# Blit bbx of tid from srcImg to empty cropFromSrcImg of bbx size.
+				cropFromSrcImg = pygame.Surface(sz, pygame.SRCALPHA, 32)
+				cropFromSrcImg.blit(srcImg, (0, 0), bbxTup)
+				pygame.image.save(cropFromSrcImg, "test/img/%05d_cropFromSrc.png" % tid)
+
+				pygame.surfarray.pixels_alpha(cropFromSrcImg)[:,:] = 0
+				
+				# Fill a sprite-sized np array with tidClr based on tidPos.
+				tidClr = convertTidToClr(tidPos)
+				tidClrAr = vecTo3dArray(tidClr, sz[0], sz[1])
+
+				tidClrBbxImg = pygame.surfarray.make_surface(tidClrAr)
+				pygame.image.save(tidClrBbxImg, "test/img/%05dTidClr.png" % tid)
+
+				# Initialize spriteImg with A=0.
+				spriteImg = pygame.Surface(sz, pygame.SRCALPHA, 32)
+				pygame.surfarray.pixels_alpha(spriteImg)[:,:] = 0
+
+				# Comp tid using tidClr colorkey - makes hole at tid.
+				tidImg.set_colorkey(tidClr)
+				spriteImg.blit(tidImg, (0, 0), bbxTup)
+
+				# Invert alpha - TODO: a) quicker (GPU) inversion, ie. OPTIMIZE?
+				# b) array_alpha instead because it copies, not references?
+				pygame.surfarray.pixels_alpha(spriteImg)[:,:] = \
+					invert255V(pygame.surfarray.pixels_alpha(spriteImg)[:,:])
+				pygame.surfarray.pixels3d(spriteImg)[:,:] = \
+					pygame.surfarray.pixels3d(cropFromSrcImg)[:,:]
+				pygame.image.save(spriteImg, "test/img/%05dBgImgCB_CkeyInvAlphaOrigClr.png" % tid)
+
+				spritesThisLev.append({"spriteImg":spriteImg, "bbx":bbx, "tid":tid})
+
+				#canvas.blit(spriteImg, (bbxTup[0], bbxTup[1]))
+				#canvasLev.blit(spriteImg, (bbxTup[0], bbxTup[1]))
+
+			spritesThisFr.append(spritesThisLev)
+
+			#pygame.image.save(canvasLev, "test/img/_canvasLev%03d.%05d.png" % (lev,  fr))
+
+		#pygame.image.save(canvas, "test/img/_canvas.%05d.png" % (fr))
+		pygame.image.save(srcImg, "test/img/_src.%05d.png" % (fr))
+		renSprites(spritesThisFr, res, fr)
+
+def renSprites(spritesThisFr, res, fr):
+	print "_renSprites(): BEGIN, fr", fr
 	canvas = pygame.Surface(res, pygame.SRCALPHA, 32)
 	canvas.fill((0, 90, 0))
-
-	for lev in lrange:
-		print "Doing lev", lev, "..."
-
-		tidPosGridThisLev = tidPosGrid[lev]
-		arTidPosGridThisLev = np.array(tidPosGridThisLev)
-		#print "arTidPosGridThisLev.shape", arTidPosGridThisLev.shape
-		padTest = np.lib.pad(arTidPosGridThisLev, ((1,0), (0,1)), "constant", constant_values=-1)
-		#print "padTest.shape", padTest.shape
-		#print "arTidPosGridThisLev[0]", arTidPosGridThisLev[0]
-		#print "padTest[0]", padTest[0]
-
-		tidToSidsThisLev = tidToSids[lev]
-		tids = tidToSidsThisLev.keys()
-		tids.sort()
-
-		tidClrGrid = converTidPosGridToTidClrGrid(padTest, tids)
-		tidImg = pygame.surfarray.make_surface(tidClrGrid)
-		path = "test/img/tidImg.lev%03d.%05d.png" % (lev, fr)
-		pygame.image.save(tidImg, path)
-
-		#print "tids", tids
-		#sprites = []
-
-		# Initialize canvasLev
+	#for spritesThisLev in spritesThisFr:
+	for lev in range(len(spritesThisFr)):
+		print "_renSprites(): fr", fr, "lev", lev
+		spritesThisLev = spritesThisFr[lev]
 		canvasLev = pygame.Surface(res, pygame.SRCALPHA, 32)
 		canvasLev.fill((0, 90, 0))
-
-
-		for tidPos in range(len(tids)):
-			tid = tids[tidPos]
-			bbx = tidToSidsThisLev[tid]["bbx"]
-			sz=(bbx[1][0]-bbx[0][0], bbx[1][1]-bbx[0][1])
-
-			# +1 totally trial + error. ***********???????? maybe not needed?
+		for spriteDic in spritesThisLev:
+			bbx = spriteDic["bbx"]
 			bbxTup = (bbx[0][0]+1, bbx[0][1]+1, bbx[1][0], bbx[1][1])
-			#print "bbxTup", bbxTup
-
-			# Blit bbx of tid from srcImg to empty cropFromSrcImg of bbx size.
-			cropFromSrcImg = pygame.Surface(sz, pygame.SRCALPHA, 32)
-			cropFromSrcImg.blit(srcImg, (0, 0), bbxTup)
-			pygame.image.save(cropFromSrcImg, "test/img/%05d_cropFromSrc.png" % tid)
-
-			pygame.surfarray.pixels_alpha(cropFromSrcImg)[:,:] = 0
-			#sprites.append({"img":cropFromSrcImg, "bbx":bbx, "tid":tid})
-			
-			# Fill a sprite-sized np array with tidClr based on tidPos.
-			tidClr = convertTidToClr(tidPos)
-			tidClrAr = vecTo3dArray(tidClr, sz[0], sz[1])
-
-			tidClrBbxImg = pygame.surfarray.make_surface(tidClrAr)
-			pygame.image.save(tidClrBbxImg, "test/img/%05dTidClr.png" % tid)
-
-			# Initialize spriteImg with A=0.
-			spriteImg = pygame.Surface(sz, pygame.SRCALPHA, 32)
-			pygame.surfarray.pixels_alpha(spriteImg)[:,:] = 0
-
-			# Comp tid using tidClr colorkey - makes hole at tid.
-			tidImg.set_colorkey(tidClr)
-			spriteImg.blit(tidImg, (0, 0), bbxTup)
-
-			# Invert alpha - TODO: a) quicker (GPU) inversion, ie. OPTIMIZE?
-			# b) array_alpha instead because it copies, not references?
-			pygame.surfarray.pixels_alpha(spriteImg)[:,:] = \
-				invert255V(pygame.surfarray.pixels_alpha(spriteImg)[:,:])
-			pygame.surfarray.pixels3d(spriteImg)[:,:] = \
-				pygame.surfarray.pixels3d(cropFromSrcImg)[:,:]
-			pygame.image.save(spriteImg, "test/img/%05dBgImgCB_CkeyInvAlphaOrigClr.png" % tid)
+			spriteImg = spriteDic["spriteImg"]
 			canvas.blit(spriteImg, (bbxTup[0], bbxTup[1]))
 			canvasLev.blit(spriteImg, (bbxTup[0], bbxTup[1]))
 
 		pygame.image.save(canvasLev, "test/img/_canvasLev%03d.%05d.png" % (lev,  fr))
 
 	pygame.image.save(canvas, "test/img/_canvas.%05d.png" % (fr))
-	pygame.image.save(srcImg, "test/img/_src.%05d.png" % (fr))
+
+genAndRenSprites()
